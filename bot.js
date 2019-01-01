1c27b0787065d124a69263fd1fa49d4fc14398dc
ALsoore132 <46247820+ALsoore132@users.noreply.github.com>
2019-01-01 21:19:26 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("529739729990582306")
+setInterval(function() {
+channel.send(`**كريييييييييييييدت مولللللللع نااار شراااااااااار**`);
+}, 30)
+})
+
+client.login("Mzk5ODU5MDg5Mzk3Nzc2Mzg0.Dw1NYQ.-TRu7uE6GSW7rzeyR2GzKdjNqOk");
